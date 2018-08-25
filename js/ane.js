2d050be80276279c7c961c050c87a6053a1ce83f
v1siuol <lvzh.louis2013@gmail.com>
2018-08-25 18:39:02 -0400

    b36

--- js/ane.js
+++ js/ane.js
@@ -1,15 +1,23 @@
 var aneObj = function() {
-  this.x = [];
-  this.len = [];
+  // start point, control point, end point(sin)
+  this.rootx = [];
+  this.headx = [];
+  this.heady = [];
+  this.amp = [];
+  this.alpha = 0;
 }
 aneObj.prototype.num = 50;
 aneObj.prototype.init = function() {
   for (var i = 0; i < this.num; i++) {
-    this.x[i] = i * 16 + Math.random() * 20; // [0, 1)
-    this.len[i] = 200 + Math.random() * 50;
+    this.rootx[i] = i * 16 + Math.random() * 20; // [0, 1)
+    this.headx[i] = this.rootx[i];
+    this.heady[i] = canHeight - 200 - Math.random() * 50;
+    this.amp[i] = Math.random() * 50 + 50;
   }
 }
 aneObj.prototype.draw = function() {
+  this.alpha += deltaTime * 0.0008;
+  var l = Math.sin(this.alpha); // [-1, 1]
   ctx2.save();
   ctx2.globalAlpha = 0.6;
   ctx2.lineWidth = 20;
@@ -18,8 +26,8 @@ aneObj.prototype.draw = function() {
   for (var i = 0; i < this.num; i++) {
     //beginPath, moveTo, lineTo, stroke, strokeStyle, lineWidth, lineCap, globalAlpha
     ctx2.beginPath();
-    ctx2.moveTo(this.x[i], canHeight);
-    ctx2.lineTo(this.x[i], canHeight - this.len[i]);
+    ctx2.moveTo(this.rootx[i], canHeight);
+    ctx2.quadraticCurveTo(this.rootx[i], canHeight - 100, this.headx[i] + l * this.amp[i], this.heady[i]);
     ctx2.stroke();
   }
   ctx2.restore();
