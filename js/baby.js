c204eb713ae5ab8e07cc4d4b88be61c4f6d9f52c
v1siuol <lvzh.louis2013@gmail.com>
2018-08-25 16:23:38 -0400

    b14

--- js/baby.js
+++ js/baby.js
@@ -2,7 +2,6 @@ var babyObj = function() {
   this.x;
   this.y;
   this.angle;
-  this.babyBody = new Image();
 
   this.babyTailTimer = 0;
   this.babyTailCount = 0;
@@ -10,12 +9,14 @@ var babyObj = function() {
   this.babyEyeTimer = 0;
   this.babyEyeCount = 0;
   this.babyEyeInterval = 1000;
+
+  this.babyBodyTimer = 0;
+  this.babyBodyCount = 0;
 }
 babyObj.prototype.init = function() {
   this.x = canWidth * 0.5 - 50;
   this.y = canHeight * 0.5 + 50;
   this.angle = 0;
-  this.babyBody.src = "./src/babyFade0.png";
 }
 
 babyObj.prototype.draw = function() {
@@ -53,13 +54,26 @@ babyObj.prototype.draw = function() {
     }
   }
 
+  // baby body
+  this.babyBodyTimer += deltaTime;
+  if (this.babyBodyTimer > 300) {
+    this.babyBodyCount++;
+    this.babyBodyTimer %= 300;
+    if (this.babyBodyCount > 19) {
+      this.babyBodyCount = 19;
+      // game over
+    }
+  }
+
   ctx1.save();
   ctx1.translate(this.x, this.y);
   ctx1.rotate(this.angle);
 
   var babyTailCount = this.babyTailCount;
   ctx1.drawImage(babyTail[babyTailCount], -babyTail[babyTailCount].width * 0.5 + 23, -babyTail[babyTailCount].height * 0.5);
-  ctx1.drawImage(this.babyBody, -this.babyBody.width * 0.5, -this.babyBody.height * 0.5);
+
+  var babyBodyCount = this.babyBodyCount;
+  ctx1.drawImage(babyBody[babyBodyCount], -babyBody[babyBodyCount].width * 0.5, -babyBody[babyBodyCount].height * 0.5);
 
   var babyEyeCount = this.babyEyeCount;
   ctx1.drawImage(babyEye[babyEyeCount], -babyEye[babyEyeCount].width * 0.5, -babyEye[babyEyeCount].height * 0.5);
