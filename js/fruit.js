8265e15f42472fce87467d2b6fcb48646ada4e28
v1siuol <lvzh.louis2013@gmail.com>
2018-08-25 14:48:41 -0400

    a25

--- js/fruit.js
+++ js/fruit.js
@@ -2,6 +2,8 @@ var fruitObj = function() {
   this.alive = [];
   this.x = [];
   this.y = [];
+  this.l = [];
+  this.spd = [];
   this.orange = new Image();
   this.blue = new Image();
 }
@@ -11,6 +13,7 @@ fruitObj.prototype.init = function() {
     this.alive[i] = true;
     this.x[i] = 0;
     this.y[i] = 0;
+    this.spd[i] = Math.random() * 0.01 + 0.005; // [0.005, 0.015)
     this.born(i);
   }
   this.orange.src = "./src/fruit.png";
@@ -20,7 +23,17 @@ fruitObj.prototype.draw = function() {
   for (var i = 0; i < this.num; i++) {
     // draw
     // find an ane, grow, fly up
-    ctx2.drawImage(this.orange, this.x[i] - this.orange.width * 0.5, this.y[i] - this.orange.height * 0.5);
+    if (this.alive[i]) {
+      if (this.l[i] <= 14) {
+        this.l[i] += this.spd[i] * deltaTime;
+      } else {
+        this.y[i] -= this.spd[i] * 7 * deltaTime;
+      }
+      ctx2.drawImage(this.orange, this.x[i] - this.l[i] * 0.5, this.y[i] - this.l[i] * 0.5, this.l[i], this.l[i]);
+      if (this.y[i] < 10) {
+        this.alive[i] = false;
+      }
+    }
   }
 }
 
@@ -28,4 +41,5 @@ fruitObj.prototype.born = function(i) {
   var aneID = Math.floor(Math.random() * ane.num);
   this.x[i] = ane.x[aneID];
   this.y[i] = canHeight - ane.len[aneID];
+  this.l[i] = 0;
 }
